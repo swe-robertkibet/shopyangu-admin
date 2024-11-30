b34d84e42da2bb4ab8b351bc3b68856ea08157cc
swe-robertkibet <swe.robertkibet@gmail.com>
2024-11-30 14:50:08 +0300

    update sidebar and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import Sidebar from "@/components/layout/Sidebar";
 import Header from "@/components/layout/Header";
-import "../globals.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
